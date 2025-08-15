b082fd97686272386b6b7ed637eb80f7a397cdd8
Avanthika <avanthikasanil6@gmail.com>
2025-08-15 14:20:03 +0530

    Add

--- src/components/NaveBar.jsx
+++ src/components/NaveBar.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './NaveBar.css';
 import Flag from './images/flah.jpeg';
 
-
-
 function NaveBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
             <div className="nav">
                 <img src={Flag} alt="indian flag" />
                 <h1>HAPPY INDEPENDENCE DAY</h1>
-                <div className="nav-list">
-                    <a href="#home">Home</a>
-                    <a href="#events">Events</a>
-                    <a href="#theme">Theme</a>
-                    <a href="#about">About Us</a>
+
+                {/* Hamburger Icon */}
+                <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </div>
+
+                {/* Nav Links */}
+                <div className={`nav-list ${menuOpen ? "active" : ""}`}>
+                    <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
+                    <a href="#events" onClick={() => setMenuOpen(false)}>Events</a>
+                    <a href="#theme" onClick={() => setMenuOpen(false)}>Theme</a>
+                    <a href="#about" onClick={() => setMenuOpen(false)}>About Us</a>
                 </div>
             </div>
         </>
-
     );
 }
+
 export default NaveBar;
